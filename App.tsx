7baa4e8967d048ab03bce41509878ec0948987f0
giahuy210903 <143804067+giahuy210903@users.noreply.github.com>
2023-10-20 22:54:44 +0700

    test

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import TestFont from './src/screens/TestFont'
+import Login from './src/screens/Login'
 
 const App = () => {
   return (
     <View>
       {/* <Text>App</Text> */}
-      <TestFont />
+      {/* <TestFont /> */}
+      <Login />
     </View>
   )
 }
---
+++ src/screens/Login.tsx
@@ -0,0 +1,81 @@
+import { StyleSheet, Text, View, Image, } from 'react-native'
+import React from 'react'
+import { COLORS, FONT_FAMILY } from '../themes/theme'
+
+
+const Login = () => {
+    return (
+        <View style={styles.container}>
+            <Image source={require('../../assets/images/LogoApp.png')}
+            style={styles.logo}></Image>       
+            <Text style={styles.login}>Đăng Nhập</Text>
+            
+            <View style={styles.nhap}>
+            <Text style={styles.sodienthoai}>Số điện thoại </Text>
+           <View style={styles.phonenumber}>
+            <View style = {styles.input}></View>
+
+           </View>
+            </View>
+       
+        </View>
+    )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: COLORS.Blue_BG,
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+       
+    },
+    login:{
+        fontFamily: FONT_FAMILY.exo2_bold,
+        fontSize: 32,
+        color: COLORS.Black,
+        alignSelf: 'center',
+        width: 163,
+        height: 38,
+    },
+    sodienthoai:{
+        fontFamily: FONT_FAMILY.exo2_bold,
+        fontSize: 16,
+        color: COLORS.Black,
+        width: 96,
+        height: 19,
+    },
+    testFont:{
+        fontFamily: FONT_FAMILY.exo2_medium,
+        fontSize: 20,
+        color: COLORS.MainBlue,
+        marginTop: 15,
+    },
+    logo:{
+        borderRadius: 35,
+        marginTop: 37,
+        width: 80,
+        height: 80,
+    },
+    nhap:{
+        paddingVertical: 35,
+        paddingHorizontal: 35,
+        width: 403,
+        height: 215,
+    },
+    phonenumber:{
+        paddingHorizontal: 27,
+        width: 403,
+        height: 55,
+        borderColor:COLORS.Black,
+    },
+    input:{
+        paddingVertical: 35,
+        paddingHorizontal: 35,
+        width: 147.67,
+        height: 22,
+    },
+    
+
+})
