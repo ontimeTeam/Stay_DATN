18028b800b73f4eccb205ed1c5c6f2cfa202dd00
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2023-10-20 11:27:00 +0700

    change
    test font, color, image and edit file location

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import TestFont from './assets/screens/TestFont'
+import TestFont from './src/screens/TestFont'
 
 const App = () => {
   return (
