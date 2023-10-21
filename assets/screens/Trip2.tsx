c3be6a9d17906b8c0685bac28094cb1e5d29a1ff
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2023-10-21 21:40:32 +0700

    trip screen
    test

---
+++ assets/screens/Trip2.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Trip2 = () => {
+    return (
+        <View>
+            <Text style={styles.header}>Trip2</Text>
+        </View>
+    )
+}
+
+export default Trip2
+
+const styles = StyleSheet.create({
+
+})
