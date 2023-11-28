e1c633a69206378f5aeb053327d9b27f15108261
Nguyễn Bảo Ngọc <ngbaongocit.4503@gmail.com>
2023-11-29 00:41:26 +0700

    splash oke, tab-view như shit. Không lên

--- App.tsx
+++ App.tsx
@@ -2,19 +2,19 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import TestFont from './src/screens/TestFont'
 import SplashScreen from './src/screens/SplashScreen'
+import Trip from './src/screens/Trip'
 
 const App = () => {
-  return (
-    <View>
-      {/* <Text>App</Text> */}
-      <TestFont />
-      {/* <SplashScreen/> */}
-    </View>
-  )
+    return (
+        <View>
+            {/* <SplashScreen/> */}
+            <Trip />
+        </View>
+    )
 }
 
 export default App
 
 const styles = StyleSheet.create({
-  
+
 })
